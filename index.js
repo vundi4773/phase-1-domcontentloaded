1db16191bc30bee5abc1f94755262a2051724c47
vundi4773 <vundi4773@gmail.com>
2024-04-27 14:58:06 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function () {
+  var text = document.getElementById("text");
+  text.textContent = "This is really cool!";
+});
+console.log("text");
